03992af77de0f238caf298329b6fc16369510e4e
Brooke Kullberg <brookekullberg@gmail.com>
2020-03-02 21:26:54 -0800

    update HomeController to add a Sender value

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -15,6 +15,7 @@ namespace FriendLetter.Controllers
     public ActionResult Letter() { 
       LetterVariable myLetterVariable = new LetterVariable();
       myLetterVariable.Recipient = "Lina";
+      myLetterVariable.Sender = "Jasmine";
       return View(myLetterVariable);
      }
   }
